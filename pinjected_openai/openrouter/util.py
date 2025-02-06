082063b9c298a0a8020f370e3d4007e6caff4e11
proboscis <nameissoap@gmail.com>
2025-02-06 15:19:22 +0900

    fix openrouter

--- pinjected_openai/openrouter/util.py
+++ pinjected_openai/openrouter/util.py
@@ -214,8 +214,8 @@ async def a_openrouter_chat_completion(
     from pprint import pformat
     logger.debug(f"payload:{pformat(payload)}")
     res = await a_openrouter_post(payload)
-    if 'choices' not in res:
-        raise RuntimeError(f"Error in response: {res}")
+    if 'error' in res:
+        raise RuntimeError(f"Error in response: {pformat(res)}")
     cost_dict = openrouter_model_table.pricing(model).calc_cost_dict(res['usage'])
     openrouter_state['cumulative_cost'] = openrouter_state.get('cumulative_cost', 0) + sum(cost_dict.values())
     logger.debug(f"response:{pformat(res)}")
